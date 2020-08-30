ebd7924df7ecc7724453717c40b47f045e4a0b69
Andrew Wei <andrewwei98@gmail.com>
2020-08-30 16:56:03 -0700

    add basic spam snorkel code

--- download_data.sh
+++ download_data.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 
 # Check that we are running from the right directory.
-#if [ ! "${PWD##*/}" = "spam" ]; then
-#    echo "Script must be run from spam directory" >&2
-#    exit 1
-#fi
+# if [ ! "${PWD##*/}" = "spam" ]; then
+#     echo "Script must be run from spam directory" >&2
+#     exit 1
+# fi
 
 FILES=( "Youtube01-Psy.csv" "Youtube02-KatyPerry.csv" "Youtube03-LMFAO.csv" "Youtube04-Eminem.csv" "Youtube05-Shakira.csv" )
 DATA_URL="http://archive.ics.uci.edu/ml/machine-learning-databases/00380/YouTube-Spam-Collection-v1.zip"
---
+++ spam-detection.py
@@ -0,0 +1,141 @@
+from utils import load_spam_dataset
+
+df_train, df_test = load_spam_dataset()
+
+# We pull out the label vectors for ease of use later
+Y_test = df_test.label.values
+
+# For clarity, we define constants to represent the class labels for spam, ham, and abstaining.
+ABSTAIN = -1
+HAM = 0
+SPAM = 1
+
+from snorkel.labeling import PandasLFApplier
+from snorkel.labeling import labeling_function
+
+import re
+
+
+@labeling_function()
+def regex_check_out(x):
+    return SPAM if re.search(r"check.*out", x.text, flags=re.I) else ABSTAIN
+
+from snorkel.preprocess import preprocessor
+from textblob import TextBlob
+
+@preprocessor(memoize=True)
+def textblob_sentiment(x):
+    scores = TextBlob(x.text)
+    x.polarity = scores.sentiment.polarity
+    x.subjectivity = scores.sentiment.subjectivity
+    return x
+
+@labeling_function(pre=[textblob_sentiment])
+def textblob_polarity(x):
+    return HAM if x.polarity > 0.9 else ABSTAIN
+
+@labeling_function(pre=[textblob_sentiment])
+def textblob_subjectivity(x):
+    return HAM if x.subjectivity >= 0.5 else ABSTAIN
+
+from snorkel.labeling import LabelingFunction
+
+
+def keyword_lookup(x, keywords, label):
+    if any(word in x.text.lower() for word in keywords):
+        return label
+    return ABSTAIN
+
+
+def make_keyword_lf(keywords, label=SPAM):
+    return LabelingFunction(
+        name=f"keyword_{keywords[0]}",
+        f=keyword_lookup,
+        resources=dict(keywords=keywords, label=label),
+    )
+
+
+"""Spam comments talk about 'my channel', 'my video', etc."""
+keyword_my = make_keyword_lf(keywords=["my"])
+
+"""Spam comments ask users to subscribe to their channels."""
+keyword_subscribe = make_keyword_lf(keywords=["subscribe"])
+
+"""Spam comments post links to other channels."""
+keyword_link = make_keyword_lf(keywords=["http"])
+
+"""Spam comments make requests rather than commenting."""
+keyword_please = make_keyword_lf(keywords=["please", "plz"])
+
+"""Ham comments actually talk about the video's content."""
+keyword_song = make_keyword_lf(keywords=["song"], label=HAM)
+
+@labeling_function()
+def short_comment(x):
+    """Ham comments are often short, such as 'cool video!'"""
+    return HAM if len(x.text.split()) < 5 else ABSTAIN
+
+from snorkel.labeling.lf.nlp import nlp_labeling_function
+
+
+@nlp_labeling_function()
+def has_person_nlp(x):
+    """Ham comments mention specific people and are short."""
+    if len(x.doc) < 20 and any([ent.label_ == "PERSON" for ent in x.doc.ents]):
+        return HAM
+    else:
+        return ABSTAIN
+
+
+# Combine all labeling functions going to use
+lfs = [
+    keyword_my,
+    keyword_subscribe,
+    keyword_link,
+    keyword_please,
+    keyword_song,
+    regex_check_out,
+    short_comment,
+    has_person_nlp,
+    textblob_polarity,
+    textblob_subjectivity,
+]
+
+applier = PandasLFApplier(lfs=lfs)
+L_train = applier.apply(df=df_train)
+
+# Use Label Model to combined input data
+from snorkel.labeling.model import LabelModel
+
+label_model = LabelModel(cardinality=2, verbose=True)
+label_model.fit(L_train=L_train, n_epochs=500, log_freq=100, seed=123)
+
+# Make predictions
+probs_train = label_model.predict_proba(L=L_train)
+
+# Filter abstained inputs
+from snorkel.labeling import filter_unlabeled_dataframe
+
+df_train_filtered, probs_train_filtered = filter_unlabeled_dataframe(
+    X=df_train, y=probs_train, L=L_train
+)
+
+# Represent each data point as a one-hot vector
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer(ngram_range=(1, 5))
+X_train = vectorizer.fit_transform(df_train_filtered.text.tolist())
+X_test = vectorizer.transform(df_test.text.tolist())
+
+# Turn probs into preds
+from snorkel.utils import probs_to_preds
+
+preds_train_filtered = probs_to_preds(probs=probs_train_filtered)
+
+# Train logistic regression model
+from sklearn.linear_model import LogisticRegression
+
+sklearn_model = LogisticRegression(C=1e3, solver="liblinear")
+sklearn_model.fit(X=X_train, y=preds_train_filtered)
+
+print(f"Test Accuracy: {sklearn_model.score(X=X_test, y=Y_test) * 100:.1f}%")
