b538e4c6a9a321b1bf9dbdd06e4b5e0780a14246
Andrew Wei <andrewwei98@gmail.com>
2020-08-31 11:59:54 -0700

    add template to flask app

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from joblib import load
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'hello'
+def my_form():
+    return render_template('input-form.html')
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    model = load('filename.joblib') 
+    return model.predict([text])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
